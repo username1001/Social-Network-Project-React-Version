79a332943febcb82f6009b572fa763c4d1d4ca0d
Amrit Pandey <spanit007@gmail.com>
2017-08-06 16:05:56 +0530

    Changed the image-webpack-loader outputpath

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
          {
             test: /\.(jpe?g|png|gif|svg)$/i,
             loaders: [
-               'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+               'file-loader?hash=sha512&digest=hex&name=[name].[ext]&outputPath=build/images/',
                'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
             ]
          }
