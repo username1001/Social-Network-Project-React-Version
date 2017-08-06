48e577e69e5aeb2cb18e2f28e90da6c50d7d38ce
Amrit Pandey <spanit007@gmail.com>
2017-08-06 15:09:31 +0530

    Added url file and image loaders in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
          {
             test: /\.scss$/,
             loader: ['style-loader', 'css-loader', 'sass-loader']
+         },
+         {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+               'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+               'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
          }
       ]
    }
